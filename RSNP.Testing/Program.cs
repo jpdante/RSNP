345eba001c01f85d4672bd9b2413455fea87f180
João Pedro <joaopdante@gmail.com>
2018-10-13 01:56:29 -0300

    Add testing project

---
+++ RSNP.Testing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RSNP.Testing {
+    class Program {
+        static void Main(string[] args) {
+            new Program().Start().Wait().Dispose();
+        }
+
+        public Program Start() {
+            return this;
+        }
+
+        public Program Wait() {
+            return this;
+        }
+
+        public void Dispose() {
+
+        }
+    }
+}
